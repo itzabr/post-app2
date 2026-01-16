fd3b69fc7bf979581df2d40950e14f3170d85db2
itzabr <adityabhushanray5@gmail.com>
2026-01-16 17:38:00 +0530

    fix db connection

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,11 @@ const userModel = require("../models/user");
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL:
+  process.env.NODE_ENV === "production"
+    ? "https://postapp-nakf.onrender.com/auth/google/callback"
+    : "http://localhost:3000/auth/google/callback"
+
 }, async (accessToken, refreshToken, profile, done) => {
 
   try {
